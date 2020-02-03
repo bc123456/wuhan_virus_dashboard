4377f01912e6583ced8274d5b74d460cfc97bcc0
Brian Chan <brianchanmy@gmail.com>
2020-02-03 22:14:43 +0800

    added cases df and awaiting df

--- assets/src/webscraper.py
+++ assets/src/webscraper.py
@@ -7,6 +7,7 @@ Created on Mon Feb  3 12:50:18 2020
 
 from lxml import html
 import requests
+import pandas as pd
 
 def fetch_highrisk():
     """
@@ -31,37 +32,50 @@ def fetch_highrisk():
 
 def fetch_cases():
     """
-    Returns a list of dictionary that contains the confirmed cases information.
-    
+    Returns a DataFrame that contains the confirmed cases information.
+
     """
     page = requests.get('https://wars.vote4.hk/en/cases')
     tree = html.fromstring(page.content)
-    
+
     boxes = tree.xpath('//*[@id="gatsby-focus-wrapper"]/div/main/div[2]/div[contains(@class, "CaseCard__WarsCaseContainer-zltyy4-0")]')
-    
+
     res = []
-    
+
     for box in boxes:
         s = box.xpath('./div[1]/div/text()')
         casenum = s[0]
         status = s[1]
-        age = box.xpath('./div[2]/div[1]/text()')[0]
+        age_and_gender = box.xpath('./div[2]/div[1]/text()')[0]
         date = box.xpath('./div[3]/div[1]/div[1]/text()')[0]
         residence = box.xpath('./div[3]/div[1]/div[2]/text()')[0]
         hospital = box.xpath('./div[3]/div[1]/div[3]/text()')[0]
         desc = box.xpath('./div[4]/p/text()')[0]
         
+        # clean up casenum
+        casenum = casenum.replace('#', '').replace('(Confirmed)', '').strip()
+        #get age from age_and_gender
+        age = age_and_gender.replace('Age', '').replace('Male', '').replace('Female', '').strip()
+        
+        #get gender from age_and_gender
+        gender = None
+        if 'male' in age_and_gender.lower():
+            gender = 'Male'
+        elif 'female' in age_and_gender.lower():
+            gender = 'Female'
+
         res.append({
-                'casenum': str(casenum),
-                'status': str(status),
-                'age': str(age),
-                'date': str(date),
-                'residence': str(residence),
-                'hospital': str(hospital),
-                'desc': str(desc)
-                })
-    
-    return res
+            'casenum': str(casenum),
+            'status': str(status),
+            'age': int(age),
+            'gender': str(gender),
+            'date': str(date),
+            'residence': str(residence),
+            'hospital': str(hospital),
+            'desc': str(desc)
+        })
+
+    return pd.DataFrame(res)
 
 def fetch_awaiting_time():
     """
@@ -86,17 +100,21 @@ def fetch_awaiting_time():
                 'time': str(time)
                 })
     
-    return res
+    return pd.DataFrame(res)
 
 def fetch_stat():
     """
-    Return a list of 4 numbers that represent death, confirmed, investigating and reported numbers respectively.
-    
+    Return a DataFrame that represent death, confirmed, investigating and reported numbers respectively.
+
     """
+    statnames = ['Death', 'Confirmed', 'Investigating', 'Reported']
+    
     page = requests.get('https://wars.vote4.hk/en/')
     tree = html.fromstring(page.content)
-    
+
     boxes = tree.xpath('//div[contains(@class, "pages__DailyStatsContainer-sc-6kvjaa-1")]/div')
     res = [int(box.xpath('./p[1]/text()')[0]) for box in boxes]
     
-    return res
+    df = pd.DataFrame(data=[res], columns=statnames)
+
+    return df
--- myapp.py
+++ myapp.py
@@ -3,6 +3,7 @@ sys.path.insert(1, 'assets/src')
 import os
 
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
@@ -16,6 +17,7 @@ import numpy as np
 import datetime
 
 from wuhan_functions import pop_address, get_coordinates, get_infection_stats
+from webscraper import fetch_stat, fetch_cases, fetch_awaiting_time
 
 #########################
 ## Set up Dash
@@ -32,7 +34,21 @@ server = app.server
 ## Get Data
 #########################
 
-death, confirmed, investigating, reported = get_infection_stats(r'assets/data/infection_stats.pkl')
+# 1. Stats
+try:
+	stats_df = fetch_stat()
+except Exception as e:
+	print(e)
+	print(f'Get stat data failed')
+	with open(r'assets/data/STATS.pkl', 'rb') as f:
+		stats_df = pickle.load(f)
+
+death = stats_df.loc[0, 'Death']
+confirmed = stats_df.loc[0, 'Confirmed']
+investigating = stats_df.loc[0, 'Investigating']
+reported = stats_df.loc[0, 'Reported']
+
+# 2. Addresses
 
 url = r'https://wars.vote4.hk/en/high-risk'
 hk_latitude, hk_longitude = 22.2793278, 114.1628131
@@ -67,6 +83,25 @@ for address in high_risk_addresses:
 
 with open(r'assets/data/coordinates_df.pkl', 'wb') as f:
     pickle.dump(coordinates_df,f)
+
+# 3. Cases
+try:
+	cases_df = fetch_cases()
+except Exception as e:
+	print(e)
+	print(f'Get cases data failed')
+	with open(r'assets/data/CASES.pkl', 'rb') as f:
+		cases_df = pickle.load(f)
+
+# 4. Awaiting time
+try:
+	awaiting_df = fetch_awaiting_time()
+except Exception as e:
+	print(e)
+	print(f'Get awaiting time data failed')
+	with open(r'assets/data/AWAITING.pkl', 'rb') as f:
+		awaiting_df = pickle.load(f)
+
 
 #########################
 ## Plot Map
@@ -136,7 +171,16 @@ app.layout = html.Div([
 		),
 	]),
 	dbc.Row([
-		dbc.Col([], width=4),
+		dbc.Col(
+			[
+				dash_table.DataTable(
+					data=cases_df.to_dict('records'),
+					columns=[{'id': c, 'name': c} for c in cases_df.columns],
+					style_table={'overflowX': 'scroll'}
+				)
+			], 
+			width=4
+		),
 		dbc.Col(
 			[
 				dcc.Graph(
@@ -146,6 +190,15 @@ app.layout = html.Div([
 			], 
 			width=8
 		)
+	]),
+	dbc.Row([
+		dbc.Col([
+			dash_table.DataTable(
+				id='table',
+				columns=[{"name": i, "id": i} for i in awaiting_df.columns],
+				data=awaiting_df.to_dict("rows")
+		    )
+		])
 	])
 ])
 
