7c56d4c1ca9a5ad7cd24486a894b4eaab7999df1
Chan <brian.chan3@fticonsulting.com>
2020-04-17 16:07:28 +0800

    updated sql

--- src/wuhan_functions.py
+++ src/wuhan_functions.py
@@ -1,5 +1,5 @@
 import os
-
+import pyodbc
 from geopy.geocoders import Nominatim
 import time
 import pickle
@@ -30,7 +30,7 @@ def calculate_stats(cases_df):
     )
     return df
 
-def load_address_csv(path=os.path.join(data_dir, 'ADDRESS.csv')):
+def load_address_csv(path=os.path.join(data_dir, 'ADDRESS.tsv')):
     address_df = pd.read_csv(
         path, 
         dtype={
@@ -41,11 +41,12 @@ def load_address_csv(path=os.path.join(data_dir, 'ADDRESS.csv')):
             'location_zh': str,
             'longitude': float,
             'latitude': float
-        }
+        },
+        sep='\t'
     )
     return address_df
 
-def load_awaiting_csv(path=os.path.join(data_dir, 'AWAITING.csv')):
+def load_awaiting_csv(path=os.path.join(data_dir, 'AWAITING.tsv')):
     awaiting_df = pd.read_csv(
         path,
         dtype={
@@ -59,11 +60,12 @@ def load_awaiting_csv(path=os.path.join(data_dir, 'AWAITING.csv')):
             'sub_district_zh': str,
             'topWait': str,
             'topWait_value': int
-        }
+        },
+        sep='\t'
     )
     return awaiting_df
 
-def load_cases_csv(path=os.path.join(data_dir, 'CASES.csv')):
+def load_cases_csv(path=os.path.join(data_dir, 'CASES.tsv')):
     cases_df = pd.read_csv(
         path,
         dtype={
@@ -87,12 +89,13 @@ def load_cases_csv(path=os.path.join(data_dir, 'CASES.csv')):
             'classification_zh': str,
             'classification_en': str,
             'source_url': str,
-        }
+        },
+        sep='\t'
     )
 
     return cases_df
 
-def load_high_risk_csv(path=os.path.join(data_dir, 'HIGH_RISK.csv')):
+def load_high_risk_csv(path=os.path.join(data_dir, 'HIGH_RISK.tsv')):
     high_risk_df = pd.read_csv(
         path,
         dtype={
@@ -114,7 +117,8 @@ def load_high_risk_csv(path=os.path.join(data_dir, 'HIGH_RISK.csv')):
             'sub_district_en': str,
             'sub_district_zh': str,
             'type': str,
-        }
+        },
+        sep='\t'
     )
 
     high_risk_df['end_date'] = pd.to_datetime(high_risk_df['end_date'], format='%Y-%m-%d')
@@ -122,7 +126,7 @@ def load_high_risk_csv(path=os.path.join(data_dir, 'HIGH_RISK.csv')):
 
     return high_risk_df
 
-def load_hospitals_csv(path=os.path.join(data_dir, 'HOSPITALS.csv')):
+def load_hospitals_csv(path=os.path.join(data_dir, 'HOSPITALS.tsv')):
     hospital_df = pd.read_csv(
         path,
         dtype={
@@ -131,13 +135,15 @@ def load_hospitals_csv(path=os.path.join(data_dir, 'HOSPITALS.csv')):
             'id': int,
             'latitude': float,
             'longitude': float,
-        }
+        },
+        sep='\t'
     )
+    
     return hospital_df
 
 def load_hospital_awaiting_csv(
-        hospital_path=os.path.join(data_dir, 'HOSPITALS.csv'),
-        awaiting_path=os.path.join(data_dir, 'AWAITING.csv')
+        hospital_path=os.path.join(data_dir, 'HOSPITALS.tsv'),
+        awaiting_path=os.path.join(data_dir, 'AWAITING.tsv')
     ):
     hospital_df = load_hospitals_csv(hospital_path)
     awaiting_df = load_awaiting_csv(awaiting_path)
@@ -157,7 +163,7 @@ def load_data_csv():
     cases_df = load_cases_csv()
     high_risk_df = load_high_risk_csv()
     hospital_awaiting_df = load_hospital_awaiting_csv()
-
+    stats_df = calculate_stats(cases_df)
     return cases_df, high_risk_df, stats_df, hospital_awaiting_df
 
 def convert_csv_to_pickle():
@@ -199,6 +205,89 @@ def load_data_pkl():
 
     return cases_df, high_risk_df, stats_df, hospital_awaiting_df
 
+def load_data_sql():
+    server = 'datavisualizationfti.database.windows.net'
+    database = 'myVisualizationData'
+    username = 'azureuser'
+    password = 'Pass7890'
+    driver = 'SQL Server'   
+    
+    cnxn = pyodbc.connect('DRIVER={' + driver + '};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    
+    cases_df = pd.read_sql('SELECT * FROM CASES', cnxn)
+    high_risk_df = pd.read_sql('SELECT * FROM HIGH_RISK', cnxn)
+    hospital_df = pd.read_sql('SELECT * FROM HOSPITALS', cnxn)
+    awaiting_df = pd.read_sql('SELECT * FROM AWAITING', cnxn)
+    
+    cnxn.close()
+    
+    cases_df = cases_df.astype(
+        {
+            'case_no': int,
+            'onset_date': str,
+            'confirmation_date': str,
+            'gender': str,
+            'age': int,
+            'hospital_zh': str,
+            'hospital_en': str,
+            'status': str,
+            'status_zh': str,
+            'status_en': str,
+            'type_zh': str,
+            'type_en': str,
+            'citizenship_zh': str,
+            'citizenship_en': str,
+            'detail_zh': str,
+            'detail_en': str,
+            'classification': str,
+            'classification_zh': str,
+            'classification_en': str,
+            'source_url': str,
+        }
+    )
+    
+    high_risk_df['lat'] = pd.to_numeric(high_risk_df['lat'])
+    high_risk_df['lng'] = pd.to_numeric(high_risk_df['lng'])
+    high_risk_df['start_date'] = pd.to_datetime(high_risk_df['start_date'].replace('Invalid date', None), format='%Y-%m-%d')
+    high_risk_df['end_date'] = pd.to_datetime(high_risk_df['end_date'].replace('Invalid date', None), format='%Y-%m-%d')
+    
+    hospital_df = hospital_df.astype(
+        {
+            'address': str,
+            'category': str,
+            'id': int,
+            'latitude': float,
+            'longitude': float,
+        }
+    )
+    
+    awaiting_df = awaiting_df.astype(
+        {
+            'district_en': str,
+            'district_zh': str,
+            'hospCode': str,
+            'hospTimeEn': str,
+            'name_en': str,
+            'name_zh': str,
+            'sub_district_en': str,
+            'sub_district_zh': str,
+            'topWait': str,
+            'topWait_value': int
+        }
+    )
+    
+    hospital_awaiting_df = pd.merge(
+        hospital_df[['address', 'latitude', 'longitude']],
+        awaiting_df[['name_en', 'topWait', 'topWait_value']],
+        how='left',
+        left_on='address',
+        right_on='name_en'
+    )
+    
+    stats_df = calculate_stats(cases_df)
+    
+    return cases_df, high_risk_df, stats_df, hospital_awaiting_df
+
 def load_data_live():
     awaiting_df = fetch_awaiting_time()
     cases_df = fetch_cases()
@@ -229,6 +318,12 @@ def load_data(live=False):
         except Exception as e:
             print(e)
             print(f'Load live data failed, switching to loading saved data')
+
+    try:
+        print('Trying to load data from SQL database.')
+        return load_data_sql()
+    except Exception as e:
+        print(f'Loading SQL database failed, trying to load data from pkl file.')
             
     try:
         print('Trying to load data from pickle file.')
